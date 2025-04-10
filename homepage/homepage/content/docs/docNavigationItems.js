b14db1fbf8c12f64d465bbbfac1a5127cf9071c8
Nikos Papadopoulos <nikpapas@gmail.com>
2025-04-10 14:53:54 +0100

    creates new docs entry for vanillajs installation

--- homepage/homepage/content/docs/docNavigationItems.js
+++ homepage/homepage/content/docs/docNavigationItems.js
@@ -39,6 +39,7 @@ export const docNavigationItems = [
           "react-native": 100,
           "react-native-expo": 100,
           svelte: 100,
+          vanilla: 100,
         },
       },
       {
