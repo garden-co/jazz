b401e3c9a761af494bda0e2c347fc2969154f4e0
Trisha Lim <hello@trishalim.com>
2025-05-14 11:03:40 +0100

    change to hr

--- homepage/design-system/src/components/atoms/Separator.tsx
+++ homepage/design-system/src/components/atoms/Separator.tsx
@@ -2,15 +2,8 @@ import clsx from "clsx";
 import { forwardRef } from "react";
 
 export const Separator = forwardRef<
-  HTMLDivElement,
+  HTMLHRElement,
   React.ComponentPropsWithoutRef<"hr">
 >(({ className, ...props }, ref) => {
-  return (
-    <div
-      ref={ref}
-      role="none"
-      className={clsx("border-t", className)}
-      {...props}
-    />
-  );
+  return <hr ref={ref} className={clsx("border-t", className)} {...props} />;
 });
