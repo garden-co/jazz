89057f96abad1fee1a3ef18c0ca2b28ce8a41367
Emil Sayahi <97276123+emmyoh@users.noreply.github.com>
2025-06-16 17:10:59 -0700

    merge

--- packages/jazz-expo/src/provider.tsx
+++ packages/jazz-expo/src/provider.tsx
@@ -5,9 +5,20 @@ import {
   AnyAccountSchema,
   CoValueFromRaw,
 } from "jazz-tools";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { ExpoSecureStoreAdapter } from "./storage/expo-secure-store-adapter.js";
 import { ExpoSQLiteAdapter } from "./storage/expo-sqlite-adapter.js";
+
+let jazzStorage: JazzProviderProps<AnyAccountSchema>["storage"] = undefined;
+export const clearLocalData = async () => {
+  if (
+    jazzStorage &&
+    jazzStorage !== "disabled" &&
+    "clearLocalData" in jazzStorage
+  ) {
+    await (jazzStorage as ExpoSQLiteAdapter).clearLocalData();
+  }
+};
 
 export function JazzProvider<
   S extends
@@ -17,6 +28,9 @@ export function JazzProvider<
   const storage = useMemo(() => {
     return props.storage ?? new ExpoSQLiteAdapter();
   }, [props.storage]);
+  useEffect(() => {
+    jazzStorage = storage;
+  }, [storage]);
 
   const kvStore = useMemo(() => {
     return props.kvStore ?? new ExpoSecureStoreAdapter();
