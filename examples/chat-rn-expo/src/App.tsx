a272c67a1d17b48f9e24e902eae5b07c1020cdc2
NicoR <nm.rainhart@gmail.com>
2025-07-01 12:04:20 -0300

    [TEST] Add `RNQuickCrypto` provider to `chat-rn-expo` example

--- examples/chat-rn-expo/src/App.tsx
+++ examples/chat-rn-expo/src/App.tsx
@@ -2,6 +2,7 @@ import { JazzExpoProvider } from "jazz-tools/expo";
 import React, { StrictMode } from "react";
 import { apiKey } from "./apiKey";
 import ChatScreen from "./chat";
+import { RNQuickCrypto } from "jazz-tools/expo/crypto";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         sync={{
           peer: `wss://cloud.jazz.tools/?key=${apiKey}`,
         }}
+        CryptoProvider={RNQuickCrypto}
       >
         <ChatScreen />
       </JazzExpoProvider>
