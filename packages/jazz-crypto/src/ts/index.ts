bd23aa09deca349e06d57c8cae34d8e54346a1ca
Brad Anderson <brad@sankatygroup.com>
2025-07-06 21:16:19 -0400

    feat: hook up TS & C++

--- packages/jazz-crypto/src/ts/index.ts
+++ packages/jazz-crypto/src/ts/index.ts
@@ -3,3 +3,19 @@ import type { JazzCrypto } from "./JazzCrypto.nitro";
 
 export const HybridJazzCrypto =
   NitroModules.createHybridObject<JazzCrypto>("JazzCrypto");
+
+export function no_args_return_string() {
+  return HybridJazzCrypto.no_args_return_string();
+}
+
+export function args_return_string(arg1: string) {
+  return HybridJazzCrypto.args_return_string(arg1);
+}
+
+export function no_args_return_ab() {
+  return HybridJazzCrypto.no_args_return_ab();
+}
+
+export function args_return_ab(arg1: ArrayBuffer) {
+  return HybridJazzCrypto.args_return_ab(arg1);
+}
