c939bf39453359fec4af8bcbb325d7e3fb3663b9
NicoR <nmrainhart@gmail.com>
2025-09-10 10:46:26 -0300

    feat: allow generating projects with more tasks

--- tests/stress-test/src/generate.ts
+++ tests/stress-test/src/generate.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker";
 import { MAX_PRIORITY, MIN_PRIORITY, Task, TodoProject } from "./1_schema";
+import { CoPlainText } from "jazz-tools";
 
 export function generateRandomProject(numTasks: number) {
   // Create a list of tasks
@@ -7,13 +8,26 @@ export function generateRandomProject(numTasks: number) {
 
   // Generate random tasks
   function populateTasks() {
-    for (let i = 0; i < numTasks; i++) {
-      const task = Task.create({
-        done: faker.datatype.boolean(),
-        text: faker.lorem.sentence({ min: 3, max: 8 }),
-        priority: getRandomInt(MIN_PRIORITY, MAX_PRIORITY),
-      });
-      tasks.$jazz.push(task);
+    const newTasks = Array.from({ length: numTasks }, () =>
+      Task.create(
+        {
+          done: faker.datatype.boolean(),
+          text: CoPlainText.create(
+            faker.lorem.sentence({ min: 3, max: 8 }),
+            tasks.$jazz.owner,
+          ),
+          priority: getRandomInt(MIN_PRIORITY, MAX_PRIORITY),
+        },
+        { owner: tasks.$jazz.owner },
+      ),
+    );
+    // Temporary workaround to avoid hitting the transaction size limit.
+    // Once https://github.com/garden-co/jazz/issues/2887 is implemented,
+    // this will happen automatically.
+    const chunkSize = 5000;
+    for (let i = 0; i < newTasks.length; i += chunkSize) {
+      const chunk = newTasks.slice(i, i + chunkSize);
+      tasks.$jazz.push(...chunk);
     }
   }
 
