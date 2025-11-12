e41fe4886162f74207750a0cbe281b2c439b9360
NicoR <nmrainhart@gmail.com>
2025-11-12 16:17:28 -0300

    refactor: use Schema-level resolve & permissions in Version history example

--- examples/version-history/src/schema.ts
+++ examples/version-history/src/schema.ts
@@ -5,14 +5,19 @@
 
 import { co, z } from "jazz-tools";
 
-export const Issue = co.map({
-  title: z.string(),
-  description: co.plainText(),
-  estimate: z.number(),
-  status: z.literal(["backlog", "in progress", "done"]),
-});
-
-export const Project = co.map({
-  name: z.string(),
-  issues: co.list(Issue),
-});
+export const Issue = co
+  .map({
+    title: z.string(),
+    description: co.plainText().withPermissions({
+      onInlineCreate: "sameAsContainer",
+    }),
+    estimate: z.number(),
+    status: z.literal(["backlog", "in progress", "done"]),
+  })
+  .resolved({ description: true })
+  .withPermissions({
+    onCreate(newGroup) {
+      newGroup.addMember("everyone", "writer");
+    },
+  });
+export type Issue = co.loaded<typeof Issue>;
