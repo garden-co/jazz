9fa49757f5d9ce08044a0e221229cf8a6dfdd7e0
Anselm <anselm.eickhoff@gmail.com>
2025-11-20 10:28:48 +0100

    Start writing blog post based on presentation

--- homepage/gcmp/lib/posts.tsx
+++ homepage/gcmp/lib/posts.tsx
@@ -1,6 +1,5 @@
 import * as HelloWorldPost from "@/components/blog/posts/1_helloWorld.mdx";
 import * as WhatIsJazzPost from "@/components/blog/posts/2_whatIsJazz.mdx";
-// import * as WhatWeShippedSinceSummerPost from "@/components/blog/posts/3_whatWeShippedSinceSummer.mdx";
 
 export const posts: (typeof HelloWorldPost)[] = [
   HelloWorldPost,
