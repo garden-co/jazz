ceda13a484cbe610e4504a48004ce4365decdd6a
Joe Innes <joe@joeinn.es>
2025-11-20 10:45:40 +0100

    Move Vanilla snippets into new TS typechecked snippets. Update Svelte examples with SyncConfig type

--- homepage/homepage/content/docs/code-snippets/server-side/quickstart/+layout.svelte
+++ homepage/homepage/content/docs/code-snippets/server-side/quickstart/+layout.svelte
@@ -2,8 +2,9 @@
   import { JazzSvelteProvider } from "jazz-tools/svelte";
   // @ts-expect-error Only available in a real SK app
 	import { PUBLIC_JAZZ_API_KEY } from "$env/static/public";
+  import { SyncConfig } from "jazz-tools";
   let { children } = $props();
-  const sync = { peer: `wss://cloud.jazz.tools/?key=${PUBLIC_JAZZ_API_KEY}` };
+  const sync: SyncConfig = { peer: `wss://cloud.jazz.tools/?key=${PUBLIC_JAZZ_API_KEY}` };
 </script>
 
 <JazzSvelteProvider {sync}>
