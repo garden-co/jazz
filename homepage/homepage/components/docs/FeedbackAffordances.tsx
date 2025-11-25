091c6aadfbfb4b9aef2534dc6598347499bb4955
Joe Innes <joe@joeinn.es>
2025-11-25 15:16:55 +0100

    Add feedback buttons to every article

---
+++ homepage/homepage/components/docs/FeedbackAffordances.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from 'react';
+import { track } from "@vercel/analytics";
+import { Button } from "@garden-co/design-system/src/components/atoms/Button";
+
+export function FeedbackAffordances() {
+  const [option, setOption] = useState<'helpful' | 'not_helpful' | null>(null);
+  const handleClick = (type: 'helpful' | 'not_helpful') => {
+    // If the user is changing their feedback, let them, otherwise block them from voting again
+    if (option === type) return;
+    setOption(type);
+    track('Docs feedback', { feedback: type });
+  };
+
+  return (
+    <div className="flex flex-row justify-start flex-wrap items-center gap-2 py-2">
+      <strong>Was this page helpful?</strong>
+      <div>
+        <Button
+          size="sm"
+          icon="check"
+          variant={option === 'helpful' ? 'inverted' : 'default'}
+          onClick={() => handleClick('helpful')}
+          className="rounded-r-none"
+        >
+          Yes
+        </Button>
+        <Button
+          size="sm"
+          icon="close"
+          variant={option === 'not_helpful' ? 'inverted' : 'default'}
+          onClick={() => handleClick('not_helpful')}
+          className="rounded-l-none"
+        >
+          No
+        </Button>
+      </div>
+    </div>
+  );
+}
