5196641965b0e28eaea4b8d125945ce80e13517b
Joe Innes <joe@joeinn.es>
2025-11-27 14:36:33 +0100

    WIP

--- homepage/homepage/content/docs/docNavigationItems.js
+++ homepage/homepage/content/docs/docNavigationItems.js
@@ -382,12 +382,7 @@ export const docNavigationItems = [
       {
         name: "Providers",
         href: "/docs/project-setup/providers",
-        done: {
-          react: 100,
-          "react-native": 100,
-          "react-native-expo": 100,
-          svelte: 100,
-        },
+        done: 100
       },
     ],
   },
