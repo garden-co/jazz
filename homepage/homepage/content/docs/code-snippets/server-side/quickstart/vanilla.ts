ce7288a1302a423a04edea815dc9b7026ced5658
Joe Innes <joe@joeinn.es>
2025-11-28 13:05:26 +0100

    Add vanilla to Server side quickstart

--- homepage/homepage/content/docs/code-snippets/server-side/quickstart/vanilla.ts
+++ homepage/homepage/content/docs/code-snippets/server-side/quickstart/vanilla.ts
@@ -1,13 +1,13 @@
 import { startWorker } from "jazz-tools/worker";
-import { announceBand } from "@/app/announceBandSchema";
+import { announceBand } from "./announceBandSchema";
 import { JazzFestWorkerAccount } from "./schema";
 // Bring your own server
 // @ts-expect-error No actual server
 import { someServer } from 'some-server';
 
 const { worker } = await startWorker({
-  syncServer: `wss://cloud.jazz.tools/?key=${process.env.NEXT_PUBLIC_JAZZ_API_KEY}`,
-  accountID: process.env.NEXT_PUBLIC_JAZZ_WORKER_ACCOUNT,
+  syncServer: `wss://cloud.jazz.tools/?key=${process.env.JAZZ_API_KEY}`,
+  accountID: process.env.VITE_JAZZ_WORKER_ACCOUNT,
   accountSecret: process.env.JAZZ_WORKER_SECRET,
   AccountSchema: JazzFestWorkerAccount,
 });
