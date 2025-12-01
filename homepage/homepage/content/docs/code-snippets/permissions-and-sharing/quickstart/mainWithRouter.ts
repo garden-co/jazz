8f6cca514b0b3429ea104fb67fd133d4c6366197
Joe Innes <joe@joeinn.es>
2025-12-01 11:21:52 +0100

    Refactor quickstart documentation to include Vanilla framework support and simplify navigation item structure

---
+++ homepage/homepage/content/docs/code-snippets/permissions-and-sharing/quickstart/mainWithRouter.ts
@@ -0,0 +1,52 @@
+import { JazzBrowserContextManager } from "jazz-tools/browser";
+import { Festival, JazzFestAccount } from "./schema";
+import { FestivalComponent } from "./FestivalComponent";
+import { NewBandComponent } from "./NewBandComponent";
+import { assertLoaded } from "jazz-tools";
+
+// @ts-expect-error Not a real vite app
+const apiKey = import.meta.env.VITE_JAZZ_API_KEY;
+const contextManager = new JazzBrowserContextManager();
+await contextManager.createContext({
+  sync: {
+    peer: `wss://cloud.jazz.tools?key=${apiKey}`,
+  },
+});
+
+function getCurrentAccount() {
+  const context = contextManager.getCurrentValue();
+  if (!context || !("me" in context)) {
+    throw new Error("");
+  }
+
+  return context.me;
+}
+
+// @ts-expect-error This is baseline now: https://web.dev/blog/baseline-urlpattern
+const festivalRoute = new URLPattern({ pathname: "/festival/:festivalId" });
+const result = festivalRoute.exec(location.href);
+let { festivalId } = result?.pathname?.groups ?? {};
+let festival;
+if (festivalId) {
+  festival = await Festival.load(festivalId);
+} else {
+  const me = getCurrentAccount();
+  const account = await JazzFestAccount.load(me.$jazz.id);
+  if (!account.$isLoaded) throw new Error("Account is not loaded");
+  account.migrate();
+  const myAccount = await account.$jazz.ensureLoaded({
+    resolve: {
+      root: {
+        myFestival: true,
+      },
+    },
+  });
+  window.location.href = `/festival/${myAccount.root.myFestival.$jazz.id}`;
+  festival = myAccount.root.myFestival;
+}
+
+assertLoaded(festival);
+const festivalComponent = FestivalComponent(festival);
+const newBand = NewBandComponent(festival);
+const app = document.querySelector<HTMLDivElement>("#app")!;
+app.append(festivalComponent, newBand);
