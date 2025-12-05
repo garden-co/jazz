c2100195f05c8912c79b499f5838b265dea9d852
Joe Innes <joe@joeinn.es>
2025-12-05 14:17:44 +0100

    Completed Auth quickstart

---
+++ homepage/homepage/content/docs/code-snippets/key-features/authentication/quickstart/AuthWithPassphrase.ts
@@ -0,0 +1,65 @@
+// [!code ++:2]
+import { PassphraseAuth } from "jazz-tools";
+import { wordlist } from "./wordlist";
+import {
+  BrowserPasskeyAuth,
+  JazzBrowserContextManager,
+} from "jazz-tools/browser";
+import { JazzFestAccount } from "./schema";
+
+export const AuthComponent = (
+  contextManager: JazzBrowserContextManager<typeof JazzFestAccount>
+) => {
+  const crypto = contextManager.getCurrentValue()?.node.crypto;
+  const authenticate = contextManager.authenticate;
+  const register = contextManager.register;
+  const authSecretStorage = contextManager.getAuthSecretStorage();
+  const appName = "JazzFest";
+  if (!crypto) throw new Error("Crypto is not available");
+
+  const auth = new BrowserPasskeyAuth(crypto, authenticate, authSecretStorage, appName);
+
+  // [!code ++:1]
+  const passphraseAuth = new PassphraseAuth(crypto, authenticate, register, authSecretStorage, wordlist)
+
+  const signUpForm = document.createElement("form");
+  const nameInput = Object.assign(document.createElement("input"), {
+    placeholder: "Name",
+    required: true,
+  });
+  const signInButton = Object.assign(document.createElement("button"), {
+    type: "button",
+    innerText: "Sign In",
+    onclick: async (evt: MouseEvent) => {
+      evt.preventDefault();
+      await auth.logIn();
+      window.location.href = "/";
+    },
+  });
+  const signUpButton = Object.assign(document.createElement("button"), {
+    type: "submit",
+    innerText: "Sign Up",
+    onclick: async (evt: MouseEvent) => {
+      evt.preventDefault();
+      await auth.signUp(nameInput.value);
+      window.location.href = "/";
+    },
+  });
+
+  // [!code ++:4]
+  const passphraseDisplay = Object.assign(document.createElement("textarea"), {
+    rows: 5,
+  });
+
+  // [!code ++:3]
+  passphraseAuth.getCurrentAccountPassphrase().then((passphrase) => {
+    passphraseDisplay.value = passphrase;
+  });
+
+  // [!code ++:1]
+  signUpForm.append(nameInput, signInButton, signUpButton, passphraseDisplay);
+  if (!authSecretStorage.isAuthenticated) {
+    return signUpForm;
+  }
+  return null;
+};
