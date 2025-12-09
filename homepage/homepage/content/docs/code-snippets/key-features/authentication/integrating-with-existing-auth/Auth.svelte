50c9d8d2d8800359fcc794216d3770e3b2251b21
Joe Innes <joe@joeinn.es>
2025-12-09 15:09:19 +0100

    Updates following https://github.com/garden-co/jazz/pull/3254

---
+++ homepage/homepage/content/docs/code-snippets/key-features/authentication/integrating-with-existing-auth/Auth.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { getJazzContext } from 'jazz-tools/svelte';
+  const context = getJazzContext();
+
+  function logIn(username: string, password: string) {
+    // Whatever your existing auth system is
+    // @ts-expect-error Virtual implementation
+    const myOldAppUser = myApp.logIn(username, password);
+    const accountID = myOldAppUser.jazzAccountID;
+    // If you've stored this in an encrypted form, make sure to decrypt it first
+    const accountSecret = myOldAppUser.jazzAccountSecret;
+    context.current.authenticate({ accountID, accountSecret });
+    // The Jazz session is now authenticated!
+  }
+</script>
