c0ad2e9da5d1d77dcc66ec59619492cf07656c7c
Guido D'Orsi <gu.dorsi@gmail.com>
2026-01-16 14:27:03 +0100

    feat: extend meteredList to include load-requests-queue type

--- packages/cojson/src/queue/LinkedList.ts
+++ packages/cojson/src/queue/LinkedList.ts
@@ -108,7 +108,7 @@ class QueueMeter {
   }
 }
 export function meteredList<T>(
-  type: "incoming" | "outgoing" | "storage-streaming",
+  type: "incoming" | "outgoing" | "storage-streaming" | "load-requests-queue",
   attrs?: Record<string, string | number>,
 ) {
   return new LinkedList<T>(new QueueMeter("jazz.messagequeue." + type, attrs));
