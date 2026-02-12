debc878d31ca627c6c683872b4fa98b18931a9a8
Antonio Musolino <antoniomusolino007@gmail.com>
2026-02-12 15:24:25 +0100

    fix: build on first run

--- examples/todo-client-localfirst-react/vite.config.ts
+++ examples/todo-client-localfirst-react/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: { target: "es2020" },
+  worker: { format: "es" },
   optimizeDeps: {
     exclude: ["groove-wasm"],
   },
