838b140f7d38ce46d9e21cd03a49521a3ab5ac58
NicoR <nmrainhart@gmail.com>
2026-02-13 09:37:59 -0300

    feat: add cross-table relations to TS example

--- examples/todo-client-localfirst-ts/schema/current.ts
+++ examples/todo-client-localfirst-ts/schema/current.ts
@@ -1,8 +1,13 @@
 import { table, col } from "jazz-ts";
 
+table("projects", {
+  name: col.string(),
+});
+
 table("todos", {
   title: col.string(),
   done: col.boolean(),
   description: col.string().optional(),
   parent: col.ref("todos").optional(),
+  project: col.ref("projects"),
 });
