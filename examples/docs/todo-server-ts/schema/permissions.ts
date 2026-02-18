9acabe1741d8596c179d43d988d86ebccae94dec
Anselm <anselm.eickhoff@gmail.com>
2026-02-17 22:00:27 -0800

    permissions: add allowedTo helpers and type inference tests

--- examples/docs/todo-server-ts/schema/permissions.ts
+++ examples/docs/todo-server-ts/schema/permissions.ts
@@ -1,11 +1,20 @@
 import { definePermissions } from "jazz-tools/permissions";
 import { app } from "./app";
 
-export default definePermissions(app, ({ policy, session }) => [
+export default definePermissions(app, ({ policy, either, both, allowedTo, session }) => [
+  // #region permissions-simple-ts
   policy.todos.allowRead.where({ owner_id: session.user_id }),
   policy.todos.allowInsert.where({ owner_id: session.user_id }),
   policy.todos.allowUpdate
-    .whereOld({ owner_id: session.user_id })
+    .whereOld(both({ owner_id: session.user_id }).and({ done: false }))
     .whereNew({ owner_id: session.user_id }),
   policy.todos.allowDelete.where({ owner_id: session.user_id }),
+  // #endregion permissions-simple-ts
+
+  // #region permissions-allowed-to-ts
+  policy.todos.allowRead.where(either({ done: false }).or(allowedTo.read("project"))),
+  policy.todos.allowUpdate
+    .whereOld(both(allowedTo.update("project")).and({ done: false }))
+    .whereNew(allowedTo.update("project")),
+  // #endregion permissions-allowed-to-ts
 ]);
