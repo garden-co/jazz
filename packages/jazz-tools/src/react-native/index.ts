fe2aa59a3993bc86285c83603ba4fd2ce6d7c01f
Antonio Musolino <antoniomusolino007@gmail.com>
2026-02-19 12:39:34 +0100

    chore: fmt fixD

--- packages/jazz-tools/src/react-native/index.ts
+++ packages/jazz-tools/src/react-native/index.ts
@@ -1,12 +1,6 @@
 export { createDb, Db, type DbConfig } from "./db.js";
-export {
-  createJazzRnRuntime,
-  type CreateJazzRnRuntimeOptions,
-} from "./create-jazz-rn-runtime.js";
-export {
-  JazzRnRuntimeAdapter,
-  type JazzRnRuntimeBinding,
-} from "./jazz-rn-runtime-adapter.js";
+export { createJazzRnRuntime, type CreateJazzRnRuntimeOptions } from "./create-jazz-rn-runtime.js";
+export { JazzRnRuntimeAdapter, type JazzRnRuntimeBinding } from "./jazz-rn-runtime-adapter.js";
 export { JazzProvider, useDb, type JazzProviderProps } from "./provider.js";
 export { useAll } from "./use-all.js";
 
