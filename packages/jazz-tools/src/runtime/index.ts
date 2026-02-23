37d383f7cb67511a12f00ad39d02cb856da29ed0
Guido D'Orsi <gu.dorsi@gmail.com>
2026-02-23 16:40:19 +0100

    revert: roll back PR58 indexed delta handling and useAll index patching

--- packages/jazz-tools/src/runtime/index.ts
+++ packages/jazz-tools/src/runtime/index.ts
@@ -19,10 +19,5 @@ export { deriveLocalPrincipalId, resolveClientSession } from "./client-session.j
 export { translateQuery } from "./query-adapter.js";
 export { transformRows, unwrapValue, type WasmValue } from "./row-transformer.js";
 export { toValue, toValueArray, toUpdateRecord } from "./value-converter.js";
-export {
-  SubscriptionManager,
-  type IndexedItem,
-  type SubscriptionDelta,
-  type UpdatedIndexedItem,
-} from "./subscription-manager.js";
+export { SubscriptionManager, type SubscriptionDelta } from "./subscription-manager.js";
 export { WorkerBridge, type WorkerBridgeOptions } from "./worker-bridge.js";
