da7814bf93c17a542301fda65bbfd2efe8f21b1e
Matteo Manchi <matteo.manchi@gmail.com>
2026-02-27 15:13:58 +0100

    fixup! formats

--- packages/inspector/src/components/data-explorer/TableDataGrid.test.tsx
+++ packages/inspector/src/components/data-explorer/TableDataGrid.test.tsx
@@ -1,4 +1,4 @@
-import { cleanup, fireEvent, render, screen, waitFor, within } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen, within } from "@testing-library/react";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { TableDataGrid } from "./TableDataGrid";
 
