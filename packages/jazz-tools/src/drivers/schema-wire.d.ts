7de6aae81e760cfc531afe49049066e6193aa66e
Anselm <anselm.eickhoff@gmail.com>
2026-02-28 11:31:36 -0800

    Remove generated jazz-tools src declaration artifacts

--- packages/jazz-tools/src/drivers/schema-wire.d.ts
+++
@@ -1,5 +0,0 @@
-import type { WasmSchema } from "./types.js";
-export declare function isWasmSchema(value: unknown): value is WasmSchema;
-export declare function serializeRuntimeSchema(schema: WasmSchema): string;
-export declare function normalizeRuntimeSchemaJson(schemaJson: string): string;
-//# sourceMappingURL=schema-wire.d.ts.map
