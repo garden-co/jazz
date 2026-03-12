f6ea5c8ef232e5bca3c2ece2a2dfca18fe60d9bc
Antonio Musolino <antoniomusolino007@gmail.com>
2026-03-12 18:54:50 +0100

    chore: docs and fix

--- examples/docs/todo-client-localfirst-expo/babel.config.cjs
+++ examples/docs/todo-client-localfirst-expo/babel.config.cjs
@@ -2,5 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    plugins: [
+      // Jazz codegen emits `declare` fields; the default flow-strip-types
+      // plugin rejects them unless this option is set.
+      ["@babel/plugin-transform-flow-strip-types", { allowDeclareFields: true }],
+    ],
   };
 };
