d1804f62e09116c0b28a714b85d23019c9b0f8a2
NicoR <nmrainhart@gmail.com>
2026-03-13 18:01:45 -0300

    chore: update generated app.ts files

--- packages/jazz-tools/tests/ts-dsl/fixtures/basic/app.ts
+++ packages/jazz-tools/tests/ts-dsl/fixtures/basic/app.ts
@@ -384,9 +384,7 @@ export class UserQueryBuilder<
 > implements QueryBuilder<UserSelectedWithIncludes<I, S, R>> {
   readonly _table = "users";
   readonly _schema: WasmSchema = wasmSchema;
-  declare readonly _initType: UserInit;
-  declare readonly _rowType: UserSelectedWithIncludes<I, S, R>;
-  readonly _rowType!: UserSelectedWithIncludes<I, S>;
+  readonly _rowType!: UserSelectedWithIncludes<I, S, R>;
   readonly _initType!: UserInit;
   private _conditions: Array<{ column: string; op: string; value: unknown }> = [];
   private _includes: Partial<UserInclude> = {};
@@ -600,7 +598,7 @@ export class ProjectQueryBuilder<
 > implements QueryBuilder<ProjectSelectedWithIncludes<I, S, R>> {
   readonly _table = "projects";
   readonly _schema: WasmSchema = wasmSchema;
-  readonly _rowType!: ProjectSelectedWithIncludes<I, S>;
+  readonly _rowType!: ProjectSelectedWithIncludes<I, S, R>;
   readonly _initType!: ProjectInit;
   private _conditions: Array<{ column: string; op: string; value: unknown }> = [];
   private _includes: Partial<ProjectInclude> = {};
@@ -814,7 +812,7 @@ export class TodoQueryBuilder<
 > implements QueryBuilder<TodoSelectedWithIncludes<I, S, R>> {
   readonly _table = "todos";
   readonly _schema: WasmSchema = wasmSchema;
-  readonly _rowType!: TodoSelectedWithIncludes<I, S>;
+  readonly _rowType!: TodoSelectedWithIncludes<I, S, R>;
   readonly _initType!: TodoInit;
   private _conditions: Array<{ column: string; op: string; value: unknown }> = [];
   private _includes: Partial<TodoInclude> = {};
