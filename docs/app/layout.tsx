e21256114018e8b1576b3648b922a27e40750727
Giordano Ricci <me@giordanoricci.com>
2026-04-22 11:14:40 -0400

    feat: analytics

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { RootProvider } from "fumadocs-ui/provider/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./global.css";
 
 const inter = Inter({
@@ -13,6 +14,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
     <html lang="en" suppressHydrationWarning className={inter.variable}>
       <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
       </body>
     </html>
   );
