128a924fa9cd802ad3752bdf8b841bcdb90888a2
Antonio Musolino <antoniomusolino007@gmail.com>
2026-06-04 10:44:07 +0200

    fix(expo): polyfill crypto getRandomValues

--- packages/jazz-tools/src/expo/polyfills.ts
+++ packages/jazz-tools/src/expo/polyfills.ts
@@ -2,8 +2,22 @@
 
 import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
 
+import { getRandomValues } from "expo-crypto";
 import { ReadableStream as PonyfillReadableStream } from "web-streams-polyfill";
 
 const readableStreamCtor = globalThis.ReadableStream ?? PonyfillReadableStream;
+const cryptoObject = globalThis.crypto;
 
 polyfillGlobal("ReadableStream", () => readableStreamCtor);
+
+if (cryptoObject) {
+  if (typeof cryptoObject.getRandomValues !== "function") {
+    Object.defineProperty(cryptoObject, "getRandomValues", {
+      configurable: true,
+      value: getRandomValues,
+      writable: true,
+    });
+  }
+} else {
+  polyfillGlobal("crypto", () => ({ getRandomValues }));
+}
