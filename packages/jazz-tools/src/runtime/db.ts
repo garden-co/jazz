44481a148ec93f8f1b137f0a2cea8134be438a8c
NicoR <nmrainhart@gmail.com>
2026-08-10 13:56:14 -0300

    feat: allow creating a standalone in-memory DB
    Ports https://github.com/garden-co/jazz/pull/1062 into the new core

--- packages/jazz-tools/src/runtime/db.ts
+++ packages/jazz-tools/src/runtime/db.ts
@@ -1049,6 +1049,7 @@ export class Db {
   }
 
   /**
+   * @internal Use {@link createDb()} instead.
    * Create a Db instance with a loaded runtime source.
    * @internal Use createDb() instead.
    */
@@ -1958,12 +1959,6 @@ export async function createDbWithRuntimeSource<RuntimeConfig extends DbConfig>(
       createRuntimeTokenOptions(ephemeralSeed, config.appId, 3600),
     );
     resolvedConfig = { ...resolvedConfig, jwtToken };
-  }
-
-  const driver = resolveStorageDriver(resolvedConfig.driver);
-
-  if (driver.type === "memory" && !resolvedConfig.serverUrl) {
-    throw new Error("driver.type='memory' requires serverUrl.");
   }
 
   const db = Db.create(resolvedConfig, runtimeSource as AnyRuntimeSource);
