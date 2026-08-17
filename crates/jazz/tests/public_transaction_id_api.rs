ad0e250321e8712668fae5a5804bf2f69b6d839e
Joe Innes <joe@joeinn.es>
2026-08-17 17:49:58 +0200

    test: require transaction id public API

---
+++ crates/jazz/tests/public_transaction_id_api.rs
@@ -0,0 +1,31 @@
+use jazz::tools::{ColumnType, OpenTransactionId, TransactionId, Value, WriteContext};
+#[cfg(feature = "runtime")]
+use jazz::tools::{DurabilityTier, JazzClient, JazzTransaction};
+
+#[test]
+fn public_api_uses_transaction_id_vocabulary() {
+    let open_transaction_id = OpenTransactionId::new();
+    let context = WriteContext::default().with_transaction_id(open_transaction_id);
+
+    assert_eq!(context.transaction_id, Some(open_transaction_id));
+    assert_eq!(context.transaction_id(), Some(open_transaction_id));
+
+    let transaction_id = "00000000000000000000000000000000"
+        .parse::<TransactionId>()
+        .expect("parse transaction id");
+    assert_eq!(
+        Value::TransactionId(*transaction_id.as_bytes()).column_type(),
+        Some(ColumnType::TransactionId),
+    );
+}
+
+#[allow(dead_code)]
+#[cfg(feature = "runtime")]
+fn transaction_api_names(
+    client: &JazzClient,
+    transaction: &JazzTransaction,
+    transaction_id: TransactionId,
+) {
+    let _: OpenTransactionId = transaction.transaction_id();
+    std::mem::drop(client.wait_for_transaction(transaction_id, DurabilityTier::Local));
+}
